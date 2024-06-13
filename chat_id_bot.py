af1d7cfcc721a85a02b49f2694a7aaf8f2db23cd
Framework8 <109827575+Framework8@users.noreply.github.com>
2024-06-13 18:58:01 +0200

    Update chat_id_bot.py

--- chat_id_bot.py
+++ chat_id_bot.py
@@ -19,6 +19,30 @@ def chat_id(message):
     chat_id=message.chat.id
     bot.send_message(chat_id, chat_id)
     print(type(chat_id))
+    user_name = message.from_user.first_name
+    contenuto = f'{user_name}:{chat_id}'
+    
+    # Legge il file e controlla se il chat_id è già presente
+    opzioni_chat = []
+    chat_id_presente = False
+    try:
+        with open(x, 'r') as file:
+            for line in file:
+                nome, id_chat = line.strip().split(':')
+                opzioni_chat.append((nome, id_chat))
+                if str(chat_id) == id_chat:
+                    chat_id_presente = True
+                    break
+    except Exception as e:
+        print(f'Errore nella lettura del file: {e}')
+
+    # Se il chat_id non è presente, aggiungi l'utente al file
+    if not chat_id_presente:
+        try:
+            with open(x, 'a') as file:
+                file.write('\n' + contenuto)
+        except Exception as e:
+            print(f'Errore nella scrittura del file: {e}')
 
 #risponde in questo modo a qualisasi messaggio non sia /chat_id; /start; /help
 @bot.message_handler(func=lambda message: True)
